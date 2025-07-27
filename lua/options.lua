67de53aaf26855fefa96a19332264a1c8d0f5b4c
minerva-jupiter <ryouturn@gmail.com>
2025-07-27 03:35:16 +0000

    add skk

--- lua/options.lua
+++ lua/options.lua
@@ -2,3 +2,10 @@ local opt = vim.opt
 
 opt.tabstop = 4
 opt.shiftwidth = 4
+
+vim.api.nvim_set_keymap('i','<C-j>','<Plug>(skkeleton-toggle)',{noremap = true})
+vim.fn['skkeleton#config']({
+	globalDictionaries = {
+		"~/.skk/SKK-JISYO.L",
+	}
+})
