018e6837a265c7cc82b300b812b0c74e5f8fb3a9
Minerva_juppiter <94231606+minerva-jupiter@users.noreply.github.com>
2025-09-24 18:20:38 +0900

    sync clipboard to os (#1)

--- lua/options.lua
+++ lua/options.lua
@@ -2,10 +2,4 @@ local opt = vim.opt
 
 opt.tabstop = 4
 opt.shiftwidth = 4
-
-vim.api.nvim_set_keymap('i','<C-j>','<Plug>(skkeleton-toggle)',{noremap = true})
-vim.fn['skkeleton#config']({
-	globalDictionaries = {
-		"~/.skk/SKK-JISYO.L",
-	}
-})
+opt.clipboard:append({"unnamedplus"})
